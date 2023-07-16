03ba9ecf2a2118d22db1b94236894e12b14add72
jum0624 <jum0624@naver.com>
2023-07-17 01:57:35 +0900

    feat: init project

---
+++ inu-phonebook/src/main/java/com/example/inuphonebook/InuPhonebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.inuphonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InuPhonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InuPhonebookApplication.class, args);
+    }
+
+}
---
+++ inu-phonebook/src/test/java/com/example/inuphonebook/InuPhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inuphonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuPhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

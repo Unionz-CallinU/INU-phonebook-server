3840947bbe2c1375c6ac8f23ec83ecdb33d7b50f
jum0624 <jum0624@naver.com>
2023-07-17 03:39:03 +0900

    add: swaggerConfig 추가

---
+++ inu-phonebook/src/main/java/com/example/inuphonebook/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.inuphonebook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.inuphonebook"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("INU 전화번호부")
+                .description("INU 전화번호부 API 명세서")
+                .version("1.0.0")
+                .build();
+    }
+}

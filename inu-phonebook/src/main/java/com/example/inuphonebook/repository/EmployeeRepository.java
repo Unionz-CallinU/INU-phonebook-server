ba0803bce8faa5f216cc353474e9f998e121da9b
jum0624 <jum0624@naver.com>
2023-07-28 01:52:44 +0900

    add: @Repository 추가

--- inu-phonebook/src/main/java/com/example/inuphonebook/repository/EmployeeRepository.java
+++ inu-phonebook/src/main/java/com/example/inuphonebook/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.inuphonebook.repository;
 
 import com.example.inuphonebook.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }

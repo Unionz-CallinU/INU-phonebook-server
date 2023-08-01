e53f5c444f8599837a024987c2100f57daa7fad7
jum0624 <jum0624@naver.com>
2023-08-01 20:02:03 +0900

    add: 전화번호부 crawling

---
+++ inu-phonebook/src/main/java/com/example/inuphonebook/controller/CrawlingController.java
@@ -0,0 +1,29 @@
+package com.example.inuphonebook.controller;
+
+import com.example.inuphonebook.common.domain.Message;
+import com.example.inuphonebook.service.INUCrawlingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/crawling")
+public class CrawlingController {
+    private final INUCrawlingService inuCrawlingService;
+
+    private static final String SUCCESS_CRAWLING_MESSAGE = "crawling 성공";
+
+    @GetMapping("/inuhomepage")
+    @ResponseStatus(HttpStatus.OK)
+    public Message savePhonebook() throws IOException {
+        inuCrawlingService.getCrawlingDatas();
+        return new Message(SUCCESS_CRAWLING_MESSAGE);
+    }
+
+}
---
+++ inu-phonebook/src/main/java/com/example/inuphonebook/service/INUCrawlingService.java
@@ -0,0 +1,49 @@
+package com.example.inuphonebook.service;
+
+import com.example.inuphonebook.domain.Employee;
+import com.example.inuphonebook.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class INUCrawlingService {
+    private final EmployeeRepository employeeRepository;
+    private static String url = "https://www.inu.ac.kr/cop/haksaStaffSearch/staffSearchView.do?id=inu_011001000000&section=all&select1=&name=";
+
+    public void getCrawlingDatas() throws IOException {
+        ArrayList<Employee> employee = new ArrayList<>();
+        Document document = Jsoup.connect(url).get();
+        Elements elements = document.select("div[class=\"tbList mgt20\"]");
+//        System.out.println(elements);
+
+        for (Element content : elements.select("tr")
+        ) {
+            Elements select = content.select("td");
+            if (select.size() != 0) {
+                Employee member = Employee.builder()
+                        .college(select.get(0).text())
+                        .department(select.get(1).text())
+                        .position(select.get(2).text())
+                        .name(select.get(3).text())
+                        .role(select.get(4).text())
+                        .phoneNumber(select.get(5).text())
+                        .email(select.get(6).text())
+                        .build();
+                System.out.println(member.getRole());
+                employee.add(member);
+            }
+        }
+        employeeRepository.saveAll(employee);
+        log.info("[INU homepage] crawling 성공");
+    }
+}

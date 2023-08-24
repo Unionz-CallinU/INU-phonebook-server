78465f1e92f001753a9b65563699abeb0661e661
jum0624 <jum0624@naver.com>
2023-08-24 18:53:14 +0900

    refactor: 직원 조회 메서드명 변경

--- inu-phonebook/src/main/java/com/example/inuphonebook/service/EmployeeService.java
+++ inu-phonebook/src/main/java/com/example/inuphonebook/service/EmployeeService.java
@@ -2,9 +2,7 @@ package com.example.inuphonebook.service;
 
 import com.example.inuphonebook.common.exception.NotFoundException;
 import com.example.inuphonebook.domain.Employee;
-import com.example.inuphonebook.dto.employee.EmployeeReqDto;
 import com.example.inuphonebook.dto.employee.EmployeeReqDto.EmployeeSearchReqDto;
-import com.example.inuphonebook.dto.employee.EmployeeRespDto;
 import com.example.inuphonebook.dto.employee.EmployeeRespDto.EmployeeDetailRespDto;
 import com.example.inuphonebook.dto.employee.EmployeeRespDto.EmployeeListRespDto;
 import com.example.inuphonebook.repository.EmployeeRepository;
@@ -21,13 +19,13 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-    public EmployeeListRespDto 직원리스트조회(EmployeeSearchReqDto employeeSearchReqDto) {
+    public EmployeeListRespDto findAllEmployee(EmployeeSearchReqDto employeeSearchReqDto) {
         List<Employee> employeeList = employeeRepository.findEmployeeList(employeeSearchReqDto.getSearch());
         return new EmployeeListRespDto(employeeList);
 
     }
 
-    public EmployeeDetailRespDto 직원상세조회(Long id) {
+    public EmployeeDetailRespDto findOneEmployee(Long id) {
         Employee employeePS = employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("해당 id의 직원이 존재하지 않습니다."));
         return new EmployeeDetailRespDto(employeePS);
     }

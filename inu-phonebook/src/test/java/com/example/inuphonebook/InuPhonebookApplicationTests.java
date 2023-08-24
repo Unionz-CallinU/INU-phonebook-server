16d7e27d8d217ad608f65427cc55ddc9059d4d41
rnwnsgud <rnwnsgud90@naver.com>
2023-08-24 19:48:58 +0900

    [fix] 테스트코드 주석

--- inu-phonebook/src/test/java/com/example/inuphonebook/InuPhonebookApplicationTests.java
+++ inu-phonebook/src/test/java/com/example/inuphonebook/InuPhonebookApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.inuphonebook;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InuPhonebookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.inuphonebook;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InuPhonebookApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

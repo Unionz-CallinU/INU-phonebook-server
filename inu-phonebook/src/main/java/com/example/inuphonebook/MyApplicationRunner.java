1f7d3f66bcb056d1a452abf68a749fe533d2cbfb
jum0624 <jum0624@naver.com>
2023-08-30 23:42:18 +0900

    fix: 이미지 크롤링 주석처리
    INU 홈페이지 크롤링 정상 작동을 확인하기 위한 수정

--- inu-phonebook/src/main/java/com/example/inuphonebook/MyApplicationRunner.java
+++ inu-phonebook/src/main/java/com/example/inuphonebook/MyApplicationRunner.java
@@ -35,74 +35,92 @@ public class MyApplicationRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        log.info("[INU] 직원정보 크롤링 시작");
         inuCrawlingService.getCrawlingDatas();
-        humanityDepartmentService.getCrawling("korean",employeeRepository);
-        humanityDepartmentService.getCrawling("english",employeeRepository);
-        humanityDepartmentService.getCrawling("german",employeeRepository);
-        humanityDepartmentService.getCrawling("uifrance",employeeRepository);
-        humanityDepartmentService.getCrawling("uijapan",employeeRepository);
-        humanityDepartmentService.getCrawling("uichina",employeeRepository);
+        log.info("[INU] 직원정보 크롤링 완료");
+//        log.info("이미지 크롤링 시작");
+//        humanityDepartmentService.getCrawling("korean",employeeRepository);
+//        humanityDepartmentService.getCrawling("english",employeeRepository);
+//        humanityDepartmentService.getCrawling("german",employeeRepository);
+//        humanityDepartmentService.getCrawling("uifrance",employeeRepository);
+//        humanityDepartmentService.getCrawling("uijapan",employeeRepository);
+//        humanityDepartmentService.getCrawling("uichina",employeeRepository);
+//        log.info("[인문대]이미지 크롤링 종료");
+//
+//        naturalScienceDepartmentService.getCrawling("math", employeeRepository);
+//        naturalScienceDepartmentService.getCrawling("physics", employeeRepository);
+//        naturalScienceDepartmentService.getCrawling("chem", employeeRepository);
+//        naturalScienceDepartmentService.getCrawling("uifashion", employeeRepository);
+//        naturalScienceDepartmentService.getCrawling("marine", employeeRepository);
+//        log.info("[자연과학대]이미지 크롤링 종료");
+//
+//        socialScienceDepartmentService.getCrawling("socialwelfare", employeeRepository);
+//        socialScienceDepartmentService.getIframe("mediaCommunication", employeeRepository);
+//        socialScienceDepartmentService.getCrawling("lis", employeeRepository);
+//        socialScienceDepartmentService.getCrawling("hrd", employeeRepository);
+//        log.info("[사회과학대]이미지 크롤링 종료");
+//
+//        globalLandscapeDepartmentService.getCrawling("uipa", employeeRepository);
+//        globalLandscapeDepartmentService.getCrawling("politics", employeeRepository);
+//        globalLandscapeDepartmentService.getCrawling("econ", employeeRepository);
+//        globalLandscapeDepartmentService.getCrawling("trade", employeeRepository);
+//        globalLandscapeDepartmentService.getCrawling("ccs", employeeRepository);
+//        log.info("[글로벌정경대]이미지 크롤링 종료");
+//
+//        engineeringDepartmentService.getCrawling_Mechanical("me", employeeRepository);
+//        engineeringDepartmentService.getCrawling("meca", employeeRepository);
+//        engineeringDepartmentService.getCrawling("elec", employeeRepository);
+//        engineeringDepartmentService.getCrawling("ee", employeeRepository);
+//        engineeringDepartmentService.getCrawling("ime", employeeRepository);
+//        engineeringDepartmentService.getCrawling("mse", employeeRepository);
+//        engineeringDepartmentService.getCrawling("safety", employeeRepository);
+//        engineeringDepartmentService.getCrawling("echeme", employeeRepository);
+//        log.info("[공과대]이미지 크롤링 종료");
+//
+//        informationTechnologyDepartmentService.getCrawling("computer", employeeRepository);
+//        informationTechnologyDepartmentService.getCrawling("communication", employeeRepository);
+//        informationTechnologyDepartmentService.getCrawlingEmbeddedMore("ese", employeeRepository);
+//        log.info("[정보기술대]이미지 크롤링 종료");
+//
+//        administrationDepartmentService.getCrawling("management", employeeRepository);
+//        administrationDepartmentService.getCrawling("tax", employeeRepository);
+//        log.info("[경영대]이미지 크롤링 종료");
+//
+//        artSportDepartmentService.getCrawling("finearts", employeeRepository);
+//        artSportDepartmentService.getCrawling_design("design", employeeRepository);
+//        artSportDepartmentService.getCrawling("uipa10", employeeRepository);
+//        artSportDepartmentService.getCrawling_sports("sports", employeeRepository);
+//        artSportDepartmentService.getCrawling("hlkn", employeeRepository);
+//        log.info("[예체대] 이미지 크롤링 종료");
+//
+//
+//        educationDepartmentService.getCrawling("edukorean", employeeRepository);
+//        educationDepartmentService.getCrawling("eduenglish", employeeRepository);
+//        educationDepartmentService.getCrawling("edujapanese", employeeRepository);
+//        educationDepartmentService.getCrawling("edumath", employeeRepository);
+//        educationDepartmentService.getCrawling("eduphysical", employeeRepository);
+//        educationDepartmentService.getCrawling("ece", employeeRepository);
+//        educationDepartmentService.getCrawling("eduhistory", employeeRepository);
+//        educationDepartmentService.getCrawling("eduethics", employeeRepository);
+//        log.info("[교육대]이미지 크롤링 종료");
+//
+//        urbanScienceDepartmentService.getCrawling("urban", employeeRepository);
+//        urbanScienceDepartmentService.getCrawling("civil", employeeRepository);
+//        urbanScienceDepartmentService.getCrawling("ucv", employeeRepository);
+//        urbanScienceDepartmentService.getCrawling("archi", employeeRepository);
+//        log.info("[도시과학대]이미지 크롤링 종료");
+//
+//        lifeScienceDepartmentService.getCrawling("life", employeeRepository);
+//        lifeScienceDepartmentService.getCrawling("bioeng", employeeRepository);
+//        lifeScienceDepartmentService.getCrawling("nanobio", employeeRepository);
+//        log.info("[생명과학대]이미지 크롤링 종료");
+//
+//        northeastAsiaDepartmentService.getCrawling("sonas", employeeRepository);
+//        log.info("[동북아]이미지 크롤링 종료");
+//
+//        lawDepartmentService.getCrawling("law", employeeRepository);
+//        log.info("[법대]이미지 크롤링 종료");
 
-        naturalScienceDepartmentService.getCrawling("math", employeeRepository);
-        naturalScienceDepartmentService.getCrawling("physics", employeeRepository);
-        naturalScienceDepartmentService.getCrawling("chem", employeeRepository);
-        naturalScienceDepartmentService.getCrawling("uifashion", employeeRepository);
-        naturalScienceDepartmentService.getCrawling("marine", employeeRepository);
-
-        socialScienceDepartmentService.getCrawling("socialwelfare", employeeRepository);
-        socialScienceDepartmentService.getIframe("mediaCommunication", employeeRepository);
-        socialScienceDepartmentService.getCrawling("lis", employeeRepository);
-        socialScienceDepartmentService.getCrawling("hrd", employeeRepository);
-
-        globalLandscapeDepartmentService.getCrawling("uipa", employeeRepository);
-        globalLandscapeDepartmentService.getCrawling("politics", employeeRepository);
-        globalLandscapeDepartmentService.getCrawling("econ", employeeRepository);
-        globalLandscapeDepartmentService.getCrawling("trade", employeeRepository);
-        globalLandscapeDepartmentService.getCrawling("ccs", employeeRepository);
-
-        engineeringDepartmentService.getCrawling_Mechanical("me", employeeRepository);
-        engineeringDepartmentService.getCrawling("meca", employeeRepository);
-        engineeringDepartmentService.getCrawling("elec", employeeRepository);
-        engineeringDepartmentService.getCrawling("ee", employeeRepository);
-        engineeringDepartmentService.getCrawling("ime", employeeRepository);
-        engineeringDepartmentService.getCrawling("mse", employeeRepository);
-        engineeringDepartmentService.getCrawling("safety", employeeRepository);
-        engineeringDepartmentService.getCrawling("echeme", employeeRepository);
-
-        informationTechnologyDepartmentService.getCrawling("computer", employeeRepository);
-        informationTechnologyDepartmentService.getCrawling("communication", employeeRepository);
-        informationTechnologyDepartmentService.getCrawlingEmbeddedMore("ese", employeeRepository);
-
-        administrationDepartmentService.getCrawling("management", employeeRepository);
-        administrationDepartmentService.getCrawling("tax", employeeRepository);
-
-        artSportDepartmentService.getCrawling("finearts", employeeRepository);
-        artSportDepartmentService.getCrawling_design("design", employeeRepository);
-        artSportDepartmentService.getCrawling("uipa10", employeeRepository);
-        artSportDepartmentService.getCrawling_sports("sports", employeeRepository);
-        artSportDepartmentService.getCrawling("hlkn", employeeRepository);
-
-        educationDepartmentService.getCrawling("edukorean", employeeRepository);
-        educationDepartmentService.getCrawling("eduenglish", employeeRepository);
-        educationDepartmentService.getCrawling("edujapanese", employeeRepository);
-        educationDepartmentService.getCrawling("edumath", employeeRepository);
-        educationDepartmentService.getCrawling("eduphysical", employeeRepository);
-        educationDepartmentService.getCrawling("ece", employeeRepository);
-        educationDepartmentService.getCrawling("eduhistory", employeeRepository);
-        educationDepartmentService.getCrawling("eduethics", employeeRepository);
-
-        urbanScienceDepartmentService.getCrawling("urban", employeeRepository);
-        urbanScienceDepartmentService.getCrawling("civil", employeeRepository);
-        urbanScienceDepartmentService.getCrawling("ucv", employeeRepository);
-        urbanScienceDepartmentService.getCrawling("archi", employeeRepository);
-
-        lifeScienceDepartmentService.getCrawling("life", employeeRepository);
-        lifeScienceDepartmentService.getCrawling("bioeng", employeeRepository);
-        lifeScienceDepartmentService.getCrawling("nanobio", employeeRepository);
-
-        northeastAsiaDepartmentService.getCrawling("sonas", employeeRepository);
-
-        lawDepartmentService.getCrawling("law", employeeRepository);
         log.debug("크롤링 실행");
 
 

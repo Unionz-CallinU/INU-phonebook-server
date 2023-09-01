b5c7983d02c50207de98f27e47729abc808ac27a
jum0624 <jum0624@naver.com>
2023-09-02 02:59:00 +0900

    fix: 홈페이지 크롤링 동적 -> 정적 크롤링 방식으로 변경

--- inu-phonebook/src/main/java/com/example/inuphonebook/service/INUCrawlingService.java
+++ inu-phonebook/src/main/java/com/example/inuphonebook/service/INUCrawlingService.java
@@ -1,15 +1,23 @@
 package com.example.inuphonebook.service;
 
-import com.example.inuphonebook.common.Util.WebDriverUtil;
+import com.example.inuphonebook.common.Util.UrlEncoder;
 import com.example.inuphonebook.domain.Employee;
 import com.example.inuphonebook.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 @Transactional
 @Service
@@ -17,20 +25,68 @@ import java.util.List;
 @Slf4j
 public class INUCrawlingService {
     private final EmployeeRepository employeeRepository;
-    private static final String url = "https://www.inu.ac.kr/inu/742/subview.do?enc=Zm5jdDF8QEB8JTJGc3RhZmZTZWFyY2glMkZpbnUlMkZzcmNoVmlldy5kbyUzRnNyY2hEZXB0VHlwZSUzRGFsbCUyNnNyY2hEZXB0JTNEJTI2bmFtZSUzRCUyNSUyNSUyNg%3D%3D";
 
+    private final String baseUrl = "https://www.inu.ac.kr/inu/742/subview.do?enc=";
+    private final String pageUrl = "/staffSearch/inu/srchView.do?srchDept=&srchDeptType=all&name=%%&page=";
     public void getCrawlingDatas() throws IOException, InterruptedException {
-        WebDriverUtil webDriverUtil = new WebDriverUtil();
-        List<Employee> employees = webDriverUtil.useDriver(url);
-        for (Employee employee : employees
-        ) {
-            Employee member = employeeRepository.findByNameAndDepartment(employee.getName(), employee.getDepartment())
-                    .orElse(null);
-            if (member != null) {
-                employees.remove(employee);
+        long page = 1;
+        try {
+            while (true) {
+                UrlEncoder Encoder = new UrlEncoder(pageUrl + page);
+                String encodingUrl = "Zm5jdDF8QEB8" + Encoder.getEncodingUrl();
+
+                String searchUrl = baseUrl + encodingUrl;
+                log.info(searchUrl);
+
+                URL url = new URL(searchUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET"); // GET 요청 보내기
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                StringBuilder content = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+                reader.close();
+
+                // 연결 종료
+                conn.disconnect();
+
+                // 데이터 처리 (파싱)
+                String htmlContent = content.toString();
+                Document document = Jsoup.parse(htmlContent);
+
+                // 원하는 요소 선택 및 내용 추출
+                ArrayList<Employee> employee = new ArrayList<>();
+
+                Elements elements = document.select("div[class=\"func-table\"]");
+
+                for (Element element : elements.select("tr")
+                ) {
+                    Elements select = element.select("td");
+                    if (select.size() != 0) {
+                        Employee member = Employee.builder()
+                                .college(select.get(0).text())
+                                .department(select.get(1).text())
+                                .position(select.get(2).text())
+                                .name(select.get(3).text())
+                                .role(select.get(4).text())
+                                .phoneNumber(select.get(5).text())
+                                .email(select.get(6).text())
+                                .build();
+                        employee.add(member);
+                    }
+                }
+                employeeRepository.saveAll(employee);
+                employee.clear();
+                page += 1;
             }
+        } catch (IndexOutOfBoundsException e) {
+            log.info("크롤링할 페이지가 더이상 존재하지 않습니다.");
         }
-        employeeRepository.saveAll(employees);
+        log.info("[INU homepage] crawling 성공");
     }
 
 }
